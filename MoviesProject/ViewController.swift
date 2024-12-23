2f0c93cff7fa6909791d6cbe3e87af24c4d1b9f0
Sunbekova <suanbekova05@gmail.com>
2024-12-23 16:21:16 +0500

    Initial Commit

---
+++ MoviesProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesProject
+//
+//  Created by Aisha Suanbekova Bakytjankyzy on 23.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
